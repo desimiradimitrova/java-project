fc6eb7c557a32f472add4ea236bbeee372ba6072
desimiradimitrova <dimitrova.desimira@gmail.com>
2023-12-19 18:47:50 +0200

    Update User entity.

--- Application/src/main/java/com/tu/application/entities/User.java
+++ Application/src/main/java/com/tu/application/entities/User.java
@@ -1,19 +1,44 @@
 package com.tu.application.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Data;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
 @Entity
-@Table(name="users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="user")
 @Data
 public class User {
-
     @Id
-    @GeneratedValue
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @NotNull
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @NotNull
+    @Column(name = "password", unique = true)
+    private String password;
+
+    @NotNull
+    @Column(name = "first_name")
     private String firstName;
+
+    @NotNull
+    @Column(name = "last_name")
     private String lastName;
+
+    @NotNull
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "roles")
+    private String userRole;
 }
